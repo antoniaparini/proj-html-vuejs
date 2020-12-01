5def2fb0ed22ed7c753241a869247ba70ba350d6
Antonia P <direiellie@gmail.com>
2020-12-01 18:25:32 +0100

    Partial 3/4

--- js/main.js
+++ js/main.js
@@ -3,10 +3,38 @@
  */
 /*console.log('js works')*/
 
-const app = new Vue({
-    el: '#app',
 
+const app = new Vue ({
+    el: '#app',
     data: {
 
+        menus:['home','about','services','packages','insight'],
+        testi:[
+            {
+                title:'Cloud Based Finance',
+                text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident.',
+                link:'Learn More',
+            },
+
+
+        ],
+    },
+    methods: {
+
+    },
+    mounted() {
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
 });
